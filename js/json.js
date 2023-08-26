9a305ba63aa6353277f8bc024f03b27d5b982728
niladhikari <niladhikari445@gmail.com>
2023-08-26 19:08:16 +0600

    add json code

---
+++ js/json.js
@@ -0,0 +1,28 @@
+const user = {id: 1, name: 'Gorib Aamir', job: 'actor'};
+// JavaScript Object Notation (JSON)
+const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
+/* 
+{ id: 1, name: 'Gorib Aamir', job: 'actor' }
+{"id":1,"name":"Gorib Aamir","job":"actor"}
+*/
+
+const shop = {
+    owner: 'Alia',
+    address: {
+        street: 'kochukhet voot er goli',
+        city: 'ranbir',
+        country: 'BD'
+    },
+    products: ['laptop', 'mic', 'monitor', 'keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false
+};
+
+const shopJson = JSON.stringify(shop);  // object to json string
+console.log(shopJson);
+
+const shopobject = JSON.parse(shopJson); // json string to object
+console.log(shopobject);
